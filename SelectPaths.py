8e594b13cce2530d5f416521d2914a537df2bb1f
dlebars <dewi.lebars@gmail.com>
2020-06-06 15:57:50 +0200

    SelectPaths.py works

--- SelectPaths.py
+++ SelectPaths.py
@@ -10,10 +10,6 @@ import pandas as pd
 import itertools
 from pathlib import Path
 
-# Options: 'ssp119', 'ssp126', 'ssp245', 'ssp370', 'ssp585'
-exp_id = ['historical', 'piControl', 'ssp119']
-var = ['zostoga'] # zostoga, zos
-
 CMIP6_path = '/nobackup_1/users/bars/synda_cmip6/CMIP6/'
 
 # The depth of paths are sometimes used later, these refer to the CMIP6 folder
@@ -22,13 +18,6 @@ st = list(filter(None, st)) # Remove empty '' strings
 di = len(st)
 
 ### Function definitions ######################################################
-
-def all_paths(data_dir)
-    '''List all paths from a given directory'''
-    list_paths = []
-    for root, dirs, files in os.walk(data_dir):
-        list_paths.append(root)
-    return list_paths
     
 def select_paths(data_dir, experiment_id, variable, ens1=False):
     '''Select all path with data for a given experiment_id and variable. 
@@ -47,7 +36,7 @@ def select_paths(data_dir, experiment_id, variable, ens1=False):
                     list_paths.append(root)
     return list_paths
 
-def ind_mod(list_paths):
+def select_ind_mod(list_paths):
     '''Takes a list of paths as input and provides a set of all the individual
     models available'''
     list_mod = []
@@ -57,17 +46,17 @@ def ind_mod(list_paths):
         list_mod.append(st[di+2])
     return set(list_mod)
 
-def select_models_intersection(experiment_id, var):
+def select_models_intersection(data_dir, experiment_id, var):
     '''Select the models that have available data for the input variable(s) and
     experiments'''
     list_com = list(itertools.product(experiment_id, var))
     for idx, val in enumerate(list_com):
-        paths = select_paths(val[0], val[1], True)
-        ind_mods = ind_mod(paths)
+        paths = select_paths(data_dir, val[0], val[1], True)
+        ind_mods = select_ind_mod(paths)
         if idx > 0:
             ind_mods = ind_mods.intersection(ind_mods_prev)
         ind_mods_prev = ind_mods
-    return ind_mods
+    return list(ind_mods)
 
 def make_info_df(list_path):
     '''Read list of path to data and store info into pandas dataframe '''
@@ -75,7 +64,7 @@ def make_info_df(list_path):
     for i in range(len(list_path)):
         st = list_path[i].split('/')
         st = list(filter(None, st)) # Remove empty '' strings
-        interm_df.iloc[i] = [ st[di+1], st[di+2], st[di+4], st[di+7], st[di+8]]
+        interm_df.loc[i] = [ st[di+1], st[di+2], st[di+4], st[di+7], st[di+8]]
     return interm_df
 
 def make_final_info_df(info_df, ind_mods):
@@ -84,29 +73,30 @@ def make_final_info_df(info_df, ind_mods):
     nb_mod = len(ind_mods)
     final_df = pd.DataFrame(columns=['Center', 'Model', 'Ensemble', 'Grid', 'Version'])
     for i in range(nb_mod):
-        info_sel_df = info_df[info_df['Model'] == ind_mod[i]]
+        info_sel_df = info_df[info_df['Model'] == ind_mods[i]]
         if len(set(info_sel_df['Ensemble'])) != 1:
-            sys.exit('ERROR: More/less than 1 ensemble name for '+ind_mod[i])
+            sys.exit('ERROR: More/less than 1 ensemble name for '+ind_mods[i])
         if len(set(info_sel_df['Grid'])) > 1:
-            print('More than one grid available for '+ind_mod[i])
+            print('More than one grid available for '+ind_mods[i])
             print(set(info_sel_df['Grid']))
             print('Using gr as default')
             Grid = 'gr'
         else:
-            Grid = info_sel_df['Grid'][0]
+            Grid = info_sel_df['Grid'].iloc[0]
         All_Versions = set(info_sel_df['Version'])
         if len(All_Versions) > 1:
-            print('More than one version available for '+ind_mod[i])
+            print('More than one version available for '+ind_mods[i])
             print(All_Versions)
             print('Using lastest version:')
             Version = sorted(All_Versions)[-1]
             print(Version)
         else:
-            Version = All_Versions
-        final_df.loc[i] = [info_sel_df['Center'].iloc[0], ind_mod[i], 
+            Version = list(All_Versions)[0]
+        final_df.loc[i] = [info_sel_df['Center'].iloc[0], ind_mods[i], 
                            info_sel_df['Ensemble'].iloc[0], Grid, Version]
     return final_df
 
+# To keep for main script:
 # def make_full_paths(data_dir, df):
 #     '''Read a pandas data frame and build the total path to data folders'''
 #     dimMod = len(df.Model)
@@ -122,9 +112,22 @@ def make_final_info_df(info_df, ind_mods):
     
 ###############################################################################
 
-ind_mods = select_models_intersection(exp_id, var)
-list_all_paths = all_paths(CMIP6_path)
-info_df = make_info_df(list_all_paths)
-final_info_df = make_final_info_df(info_df, ind_mods)
 
-final_info_df.to_csv(index=False)
+for var in ['zostoga', 'zos']:
+    for sce in ['ssp119', 'ssp126', 'ssp245', 'ssp370', 'ssp585']:
+        exp_id = ['historical', 'piControl', sce]
+
+        ind_mods = select_models_intersection(CMIP6_path, exp_id, var)
+        print(ind_mods)
+        list_all_paths = select_paths(CMIP6_path, exp_id[0], var[0], ens1=True)
+        print(list_all_paths)
+        info_df = make_info_df(list_all_paths)
+        print(info_df)
+        final_info_df = make_final_info_df(info_df, ind_mods)
+        print(final_info_df)
+        if (len(var) == 1) & (len(exp_id) == 3):
+            final_info_df.to_csv('AvailableExperiments_'+var[0]+'_'+exp_id[0]+
+                                 '_'+exp_id[1]+'_'+exp_id[2]+'.csv', index=False)
+        else:
+            print('ERROR: Output file name not compatible with length of var'+
+                  'or exp_id')
