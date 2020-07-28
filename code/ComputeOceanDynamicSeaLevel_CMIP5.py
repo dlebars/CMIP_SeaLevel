c934ba62ce2bca107b3c2b1bb441f9915fcebdcc
dlebars <dewi.lebars@gmail.com>
2020-07-28 11:53:14 +0200

    Multiple buggs fixed. Now ComputeOceanDynamicSeaLevel_CMIP5.py works for almost all rcp85 models

--- code/ComputeOceanDynamicSeaLevel_CMIP5.py
+++ code/ComputeOceanDynamicSeaLevel_CMIP5.py
@@ -15,6 +15,14 @@ import mod_loc as loc
 import pandas as pd
 import xesmf as xe
 
+def read_ModelNames(ds):
+    '''Read model names, this need a little function because characters are not
+    read properly by xarray'''
+    mod_names = []
+    for i in range(len(ds.ModelNames)):
+        mod_names.append(str(ds.ModelNames[i].values)[2:-1])
+    return mod_names
+
 VAR = 'zos'
 EXP = 'rcp85'
 
@@ -46,50 +54,52 @@ mid = (years_e + years_s)/2
 #Read the regular 1*1 grid to use for regridded outputs
 DIRgrid = '/nobackup/users/bars/SeaLevelFromHylke/CMIP5_OCEAN/Fingerprints/' # TODO: Add this standard grid somewhere to the project
 rg = xr.open_dataset(DIRgrid+'Relative_icesheets.nc')
+rg = rg.rename({'latitude':'lat', 'longitude':'lon'})
 
-dimLonOut   = len(rg.longitude)
-dimLatOut   = len(rg.latitude)
-cLatOut     = np.cos(np.deg2rad(rg.latitude))
+dimLonOut   = len(rg.lon)
+dimLatOut   = len(rg.lat)
+cLatOut     = np.cos(np.deg2rad(rg.lat))
 #Build a mask for the new grid
 MaskOut = rg.DYN_ANT
 MaskOut = MaskOut.where(MaskOut == 0, 1)
 MaskOut = MaskOut.where(MaskOut != 0)
 # Mask the Caspian sea
-MaskOut.loc[dict(latitude=slice(35,51), longitude=slice(45,56))] = np.nan
+MaskOut.loc[dict(lat=slice(35,51), lon=slice(45,56))] = np.nan
 # TODO: Should the center of Australia be also masked?
 # New mask that includes the mediterranean
 MaskOut_Med = MaskOut
-MaskOut_Med.loc[dict(latitude=slice(20.5,41.5), longitude=slice(354,360))] = np.nan
-MaskOut_Med.loc[dict(latitude=slice(20.5,41.5), longitude=slice(0,44))] = np.nan
-MaskOut_Med.loc[dict(latitude=slice(41,47.5), longitude=slice(2,44))] = np.nan
+MaskOut_Med.loc[dict(lat=slice(20.5,41.5), lon=slice(354,360))] = np.nan
+MaskOut_Med.loc[dict(lat=slice(20.5,41.5), lon=slice(0,44))] = np.nan
+MaskOut_Med.loc[dict(lat=slice(41,47.5), lon=slice(2,44))] = np.nan
 
 # Make a dataset for easy regridding with xESMF
-ds_out = xr.Dataset({'lat': (['lat'], rg.latitude),
-                     'lon': (['lon'], rg.longitude)})
+ds_out = xr.Dataset({'lat': (['lat'], rg.lat),
+                     'lon': (['lon'], rg.lon)})
 
 print("Models used:")
 print(Models)
 
 #TODO later
-fref   = xr.open_dataset(Dir_CMIP5_TE+'ReferenceZOS_ForEXP'+EXP+'_'+
-                         year_min_ref+'_'+year_max_ref+'.nc')
-ftrend = xr.open_dataset(Dir_CMIP5_TE+'TrendZOS_ForEXP'+EXP+'.nc')
+fref   = xr.open_dataset(f'{Dir_CMIP5_TE}ReferenceZOS_ForEXP{EXP}_'
+                         f'{year_min_ref}_{year_max_ref}.nc')
+ftrend = xr.open_dataset(f'{Dir_CMIP5_TE}TrendZOS_ForEXP{EXP}.nc')
 
 #Read the average zos fields to discount from the model sea level
 # ;fzos_avg = addfile("CMIP5_SeaLevel_"+EXP+"_zos_avg_1950-2100.nc","r") ;For Sanne's project
-fzos_avg = xr.open_dataset('CMIP5_SeaLevel_'+EXP+'_zos_avg_'+year_min_ref+'-2100.nc')
+fzos_avg = xr.open_dataset(f'{Dir_CMIP5_TE}CMIP5_SeaLevel_{EXP}_zos_avg_{year_min_ref}-2100.nc')
 # ;fzos_avg = addfile("CMIP5_SeaLevel_"+EXP+"_zos_avg_1900-2006.nc","r") ;For Star's historical files
 
-zos_avg_ModelNames = fzos_avg.ModelNames
+zos_avg_ModelNames = read_ModelNames(fzos_avg)
 zos_avg  = fzos_avg.AverageSeaLevel
 time_zos_avg = fzos_avg.time
 print('Check the time vector:')
 print(time_zos_avg)
-indzosref = np.where((time_zos_avg >= year_min_ref) and (time_zos_avg < year_max_ref))
-zos_avg_ref =  zos_avg(:,indzosref).mean(axis=1)
+indzosref = np.where((time_zos_avg >= year_min_ref) & (time_zos_avg < year_max_ref))[0]
+print(indzosref)
+zos_avg_ref =  zos_avg[:,indzosref].mean(axis=1)
 
-for i in range(len(ModelList.Models)):
-    print('####### Working on model '+i+','+Models[i]+'  #####')
+for i in range(28,len(ModelList.Models)):
+    print(f'####### Working on model {i}, {Models[i]}  #####')
     #### Read scenario data
     files1 = loc.select_cmip5_files(VAR, EXP, ModelList.Centers[i], 
                                 ModelList.Models[i])
@@ -98,10 +108,16 @@ for i in range(len(ModelList.Models)):
     try:
         f1 = xr.open_mfdataset(files1,combine='by_coords')
     except:
-        print('Open by_coords did not work for:'+ ModelList.Centers[i]+
-              '/'+ModelList.Models[i]+'/'+EXP)
-        print('Using nested option instead')
-        f1 = xr.open_mfdataset(files1,combine='nested', concat_dim='time')
+        try:
+            print('Open by_coords did not work for:'+ ModelList.Centers[i]+
+                  '/'+ModelList.Models[i]+'/'+EXP)
+            print('Using nested option instead')
+            f1 = xr.open_mfdataset(files1,combine='nested', concat_dim='time')
+        except:
+            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+            print(f'Could not open data from {Models[i]}')
+            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+            continue
     VAR1 = f1[VAR].squeeze()
 
     print(VAR1)
@@ -123,37 +139,50 @@ for i in range(len(ModelList.Models)):
     f2 = xr.open_mfdataset(files2,combine='by_coords')
     time2 = f2.time
     timeUT2 = time2.dt.year
-
-    lat    = f1.lat
-    lon    = f1.lon
-    print(lon)
-    print(lat)
-    dimlat = len(lat.shape)
-    dimlon = len(lon.shape)
-
-    regridder = xe.Regridder(f1, ds_out, 'bilinear')
-    print(regridder)
     
-    #TODO: This lon might not be necessary, only used in interpollation
-    if (Models(i).eq."bcc-csm1-1").or.(Models(i).eq."bcc-csm1-1-m").or. \
-     (Models(i).eq."GFDL-ESM2G").or.(Models(i).eq."GFDL-ESM2M").or. \
-     (Models(i).eq."GFDL-CM3") then
-        lon = np.where(lon<0,lon+360,lon)
+    if len(f1.lat.shape) == 1:
+        name_lat = 'lat'
+        name_lon = 'lon'
+    elif len(f1.lat.shape) == 2:
+        name_lat = 'rlat'
+        name_lon = 'rlon'        
+    
+    if Models[i] in ['IPSL-CM5A-LR','IPSL-CM5A-MR','IPSL-CM5B-LR','NorESM1-M', 
+                     'NorESM1-ME','ACCESS1-0','CCSM4','MPI-ESM-LR','MPI-ESM-MR']:
+        f1 = f1.rename({'j':name_lat, 'i':name_lon})
+        f2 = f2.rename({'j':name_lat, 'i':name_lon})
+    
+    try:
+        regridder = xe.Regridder(f1, ds_out, 'bilinear',periodic=True)
+        print(regridder)
+    except:
+        print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        print(f'Regridding did not work for {Models[i]}')
+        print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        continue
+    
+    # TODO: This lon might not be necessary, only used in interpollation
+#     if (Models(i).eq."bcc-csm1-1").or.(Models(i).eq."bcc-csm1-1-m").or. \
+#      (Models(i).eq."GFDL-ESM2G").or.(Models(i).eq."GFDL-ESM2M").or. \
+#      (Models(i).eq."GFDL-CM3") then
+#         lon = np.where(lon<0,lon+360,lon)
 
-    RefVAR1    = fref[Models(i)]
+    RefVAR1 = fref[Models[i]]
+    RefVAR1 = RefVAR1.rename({RefVAR1.dims[0]:name_lat,
+                              RefVAR1.dims[1]:name_lon})
     # Use the reference field to mask the small seas that are not connected to the
     # ocean and areas where sea ice is included on the ocean load
-    RefVAR1_corr = RefVAR1 - RefVAR1.mean
+    RefVAR1_corr = RefVAR1 - RefVAR1.mean()
     MaskRefVAR1  = np.where((RefVAR1_corr>=2) | (RefVAR1_corr<=-2),np.nan,1)
 
     MAT_CorrectedZOS_reg = np.zeros([len(years_s),dimLatOut,dimLonOut])
     
     ##### Loop on the years ######################################
-    for y in range(len(years_s)):
-        print('Working on period: '+years_s[y]+'-'+years_e[y])
-        indzossel   = np.where((time_zos_avg>=years_s[y]) & (time_zos_avg<years_e[y]))
+    for y in [0,1]: #range(len(years_s)):
+        print(f'Working on period: {years_s[y]}-{years_e[y]}')
+        indzossel   = np.where((time_zos_avg>=years_s[y]) & (time_zos_avg<years_e[y]))[0]
         if indzossel.size == 0:
-            print('No data during the period: '+years_s[y]+'-'+years_e[y])
+            print(f'No data during the period: {years_s[y]}-{years_e[y]}')
             MAT_CorrectedZOS_reg[y,:,:] = np.nan
         else:
             if len(indzossel) == 1:
@@ -162,10 +191,10 @@ for i in range(len(ModelList.Models)):
                 zos_avg_sel = zos_avg[:,indzossel].mean(axis=1)
 
             if years_s[y]>=2006:
-                ind_time_sel = np.where((timeUT>=years_s[y]) & (timeUT<=years_e[y]))
+                ind_time_sel = np.where((timeUT>=years_s[y]) & (timeUT<=years_e[y]))[0]
                 VAR1 = f1[VAR][ind_time_sel,:,:]
-            else
-                ind_time_sel = np.where((timeUT2>=years_s[y]) & (timeUT2<=years_e[y]))
+            else:
+                ind_time_sel = np.where((timeUT2>=years_s[y]) & (timeUT2<=years_e[y]))[0]
                 VAR1 = f2[VAR][ind_time_sel,:,:]
 
             if (Models[i] in ['MIROC5', 'GISS-E2-R', 'GISS-E2-R-CC', 'EC-EARTH', 
@@ -173,7 +202,7 @@ for i in range(len(ModelList.Models)):
                 VAR1 = np.where(VAR1==0,np.nan,VAR1)
 
             VAR1avg = VAR1.mean(axis=0) # Compute time average
-            ind_zos_avg = np.where(zos_avg_ModelNames==Models[i])
+            ind_zos_avg = zos_avg_ModelNames.index(Models[i])
             AnomVAR1 = (VAR1avg - RefVAR1)*100 # Convert m to cm
             AnomVAR1 = AnomVAR1*MaskRefVAR1
 
@@ -182,13 +211,16 @@ for i in range(len(ModelList.Models)):
 
             # Effective number of years to detrend: year of interest 
             # minus mean of reference period
-            nbyears = mid[y] - [year_max_ref+year_min_ref]/2
+            nbyears = mid[y] - (year_max_ref+year_min_ref)/2
 
             TrendVAR1 = ftrend[Models[i]]*nbyears*100
-            DTrendVAR1 = AnomVAR1 - TrendVAR1 - ZOS_AVG_CORR
-        
+            TrendVAR1 = TrendVAR1.rename({TrendVAR1.dims[0]:name_lat, 
+                                          TrendVAR1.dims[1]:name_lon})
+            DTrendVAR1 = AnomVAR1 - TrendVAR1 - ZOS_AVG_CORR           
+            print(DTrendVAR1)
             # Regrid to the reference 1*1 degree grid            
-            DTrendVAR1_reg = regridder(DTrendVAR1)
+            DTrendVAR1_reg = regridder(DTrendVAR1.isel(time=0)) 
+            #!!! lat/lon should be right most dimensions
 
             # Mask other problematic regions here
 #           DTrendVAR1_reg@_FillValue = 1e+20
@@ -208,7 +240,12 @@ for i in range(len(ModelList.Models)):
 #             print("Removing area mean of:" + area_mean + " cm")
 #            MAT_CorrectedZOS_reg(y,:,:) = DTrendVAR1_reg - area_mean
 
+    regridder.clean_weight_file()
     print(DTrendVAR1_reg)
+# Change the name of output dimensions
+# RefVAR1 = RefVAR1.rename({RefVAR1.dims[0]:name_lat,
+#                       RefVAR1.dims[1]:name_lon})
+
 #     MAT_CorrectedZOS_reg!0 = "time"
 #     MAT_CorrectedZOS_reg!1 = "latitude"
 #     MAT_CorrectedZOS_reg!2 = "longitude"
