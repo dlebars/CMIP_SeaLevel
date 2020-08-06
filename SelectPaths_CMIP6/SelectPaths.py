d8da0a5d906c32c2101063727c5b5c5d6f13745a
dlebars <dewi.lebars@gmail.com>
2020-08-06 16:48:31 +0200

    Remove the use of a global variable in functions

--- SelectPaths_CMIP6/SelectPaths.py
+++ SelectPaths_CMIP6/SelectPaths.py
@@ -1,7 +1,6 @@
 ###############################################################################
-# SelectPaths.py: Interaogate the local CMIP6 database and selects the paths
+# SelectPaths.py: Interogate the local CMIP6 database and select the paths
 # to relevent data.
-# To Do: Solve the di issue
 ###############################################################################
 
 import sys
@@ -10,45 +9,45 @@ import pandas as pd
 import itertools
 from pathlib import Path
 
-CMIP6_path = '/nobackup_1/users/bars/synda_cmip6/CMIP6/'
-
-# The depth of paths are sometimes used later, these refer to the CMIP6 folder
-st = CMIP6_path.split('/')
-st = list(filter(None, st)) # Remove empty '' strings
-di = len(st)
-
 ### Function definitions ######################################################
-    
+
+def depth_path(data_dir):
+    st = data_dir.split('/')
+    st = list(filter(None, st)) # Remove empty '' strings
+    return len(st)
+
 def select_paths(data_dir, experiment_id, variable, ens1=False):
     '''Select all path with data for a given experiment_id and variable. 
     Outout results in a list'''
+    depth = depth_path(data_dir)
     list_paths = []
     for root, dirs, files in os.walk(data_dir):
         if files:
             st = root.split('/')
             st = list(filter(None, st)) # Remove empty '' strings
             if ens1:
-                if (st[di+3] == experiment_id) and (st[di+6] == variable) \
-                and (st[di+4] == 'r1i1p1f1'):
+                if (st[depth+3] == experiment_id) and (st[depth+6] == variable) \
+                and (st[depth+4] == 'r1i1p1f1'):
                     list_paths.append(root)
             else:
-                if (st[di+3] == experiment_id) and (st[di+6] == variable):
+                if (st[depth+3] == experiment_id) and (st[depth+6] == variable):
                     list_paths.append(root)
     return list_paths
 
-def select_ind_mod(list_paths):
+def select_ind_mod(list_paths, depth):
     '''Takes a list of paths as input and provides a set of all the individual
     models available'''
     list_mod = []
     for path in list_paths:
         st = path.split('/')
         st = list(filter(None, st)) # Remove empty '' strings
-        list_mod.append(st[di+2])
+        list_mod.append(st[depth+2])
     return set(list_mod)
 
 def select_models_intersection(data_dir, experiment_id, var):
     '''Select the models that have available data for the input variable(s) and
     experiments'''
+    depth = depth_path(data_dir)
     # Make sure inputs are lists because itertools.product needs list inputs
     if not isinstance(var, list):
         var = [var]
@@ -57,19 +56,19 @@ def select_models_intersection(data_dir, experiment_id, var):
     list_com = list(itertools.product(experiment_id, var))
     for idx, val in enumerate(list_com):
         paths = select_paths(data_dir, val[0], val[1], True)
-        ind_mods = select_ind_mod(paths)
+        ind_mods = select_ind_mod(paths, depth)
         if idx > 0:
             ind_mods = ind_mods.intersection(ind_mods_prev)
         ind_mods_prev = ind_mods
     return list(ind_mods)
 
-def make_info_df(list_path):
+def make_info_df(list_path, depth):
     '''Read list of path to data and store info into pandas dataframe '''
     interm_df = pd.DataFrame(columns=['Center', 'Model', 'Ensemble', 'Grid', 'Version'])
     for i in range(len(list_path)):
         st = list_path[i].split('/')
         st = list(filter(None, st)) # Remove empty '' strings
-        interm_df.loc[i] = [ st[di+1], st[di+2], st[di+4], st[di+7], st[di+8]]
+        interm_df.loc[i] = [ st[depth+1], st[depth+2], st[depth+4], st[depth+7], st[depth+8]]
     return interm_df
 
 def make_final_info_df(info_df, ind_mods):
@@ -103,23 +102,11 @@ def make_final_info_df(info_df, ind_mods):
         final_df.loc[i] = [info_sel_df['Center'].iloc[0], ind_mods[i], 
                            info_sel_df['Ensemble'].iloc[0], Grid, Version]
     return final_df
-
-# To keep for main script:
-# def make_full_paths(data_dir, df):
-#     '''Read a pandas data frame and build the total path to data folders'''
-#     dimMod = len(df.Model)
-#     full_paths = []
-#     for i in range(dimMod):
-#         p = Path(data_dir+'CMIP/'+df.Center[i]+'/'+df.Model[i]+'/'+EXP[j]+
-#                  '/'+Freq)
-#         files1 = list(p.glob('*/*/*/*/'+VAR+'/*'+VAR+'*.nc'))
-        
-#         full_paths.append(?)
-        
-#     return full_paths
     
 ###############################################################################
 
+CMIP6_path = '/nobackup_1/users/bars/synda_cmip6/CMIP6/'
+depth = depth_path(CMIP6_path)
 
 for var in ['zostoga', 'zos']:
     var = [var]
@@ -132,7 +119,7 @@ for var in ['zostoga', 'zos']:
         for idx, ei in enumerate(exp_id):
             list_all_paths = select_paths(CMIP6_path, ei, var[0], ens1=True)
             #print('\n'.join(list_all_paths))
-            info_df = make_info_df(list_all_paths)
+            info_df = make_info_df(list_all_paths, depth)
             #print('Info before final selection:')
             #print(info_df)
             if idx == 0:
@@ -144,10 +131,9 @@ for var in ['zostoga', 'zos']:
         print('Final info to be saved as csv file:')
         print(final_info_df)
         if (len(var) == 1) & (len(exp_id) == 3):
-            final_info_df.to_csv('AvailableExperiments_'+var[0]+'_'+exp_id[0]+
-                                 '_'+exp_id[1]+'_'+exp_id[2]+'.csv', index=False)
+            file_name = (f'AvailableExperiments_{var[0]}_{exp_id[0]}_{exp_id[1]}'+
+                         f'_{exp_id[2]}.csv')
+            final_info_df.to_csv(file_name, index=False)
         else:
             print('ERROR: Output file name not compatible with length of var'+
                   'or exp_id')
-
-        exp_id = ['historical', 'piControl', sce]
