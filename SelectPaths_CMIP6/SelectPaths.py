36696513393288cc2f17c89cddeeacf9aa23a68c
dlebars <dewi.lebars@gmail.com>
2021-06-02 09:01:06 +0200

    Force the selection of the same variant for historical and scenario

--- SelectPaths_CMIP6/SelectPaths.py
+++ SelectPaths_CMIP6/SelectPaths.py
@@ -16,7 +16,7 @@ def depth_path(data_dir):
     st = list(filter(None, st)) # Remove empty '' strings
     return len(st)
 
-def select_paths(data_dir, experiment_id, variable, ens1=False):
+def select_paths(data_dir, experiment_id, variable, var):
     '''Select all path with data for a given experiment_id and variable. 
     Outout results in a list'''
     depth = depth_path(data_dir)
@@ -25,13 +25,14 @@ def select_paths(data_dir, experiment_id, variable, ens1=False):
         if files:
             st = root.split('/')
             st = list(filter(None, st)) # Remove empty '' strings
-            if ens1:
-                if (st[depth+3] == experiment_id) and (st[depth+6] == variable) \
-                and (st[depth+4] == 'r1i1p1f1'):
-                    list_paths.append(root)
-            else:
+            if var=='any':
                 if (st[depth+3] == experiment_id) and (st[depth+6] == variable):
                     list_paths.append(root)
+            else:
+                if (st[depth+3] == experiment_id) and (st[depth+6] == variable) \
+                and (st[depth+4] == var):
+                    list_paths.append(root)
+                
     return list_paths
 
 def select_ind_mod(list_paths, depth):
@@ -44,18 +45,18 @@ def select_ind_mod(list_paths, depth):
         list_mod.append(st[depth+2])
     return set(list_mod)
 
-def select_models_intersection(data_dir, experiment_id, var, ens1):
+def select_models_intersection(data_dir, experiment_id, variable, var):
     '''Select the models that have available data for the input variable(s) and
     experiments'''
     depth = depth_path(data_dir)
     # Make sure inputs are lists because itertools.product needs list inputs
-    if not isinstance(var, list):
-        var = [var]
+    if not isinstance(variable, list):
+        variable = [variable]
     if not isinstance(experiment_id, list):
         experiment_id = [experiment_id]
-    list_com = list(itertools.product(experiment_id, var))
+    list_com = list(itertools.product(experiment_id, variable))
     for idx, val in enumerate(list_com):
-        paths = select_paths(data_dir, val[0], val[1], ens1)
+        paths = select_paths(data_dir, val[0], val[1], var)
         ind_mods = select_ind_mod(paths, depth)
         if idx > 0:
             ind_mods = ind_mods.intersection(ind_mods_prev)
@@ -71,7 +72,7 @@ def make_info_df(list_path, depth):
         interm_df.loc[i] = [ st[depth+1], st[depth+2], st[depth+4], st[depth+7], st[depth+8]]
     return interm_df
 
-def make_final_info_df(info_df, ind_mods):
+def make_final_info_df(info_df, ind_mods, info_exp, exp):
     '''Reads dataframe output from function make_info_df and select the final
     model grid and version to use'''
     nb_mod = len(ind_mods)
@@ -79,21 +80,28 @@ def make_final_info_df(info_df, ind_mods):
     for i in range(nb_mod):
         info_sel_df = info_df[info_df['Model'] == ind_mods[i]]
         print(info_sel_df)
+        
         if len(set(info_sel_df['Variant'])) > 1:
             print('More than one variant available for '+ind_mods[i])
-        if 'r1i1p1f1' in info_sel_df['Variant'].values:
-            Variant = 'r1i1p1f1'
-        elif 'r1i1p1f2' in info_sel_df['Variant'].values:
-            Variant = 'r1i1p1f2'
-        elif 'r1i1p1f3' in info_sel_df['Variant'].values:
-            Variant = 'r1i1p1f3'
-        elif 'r1i1p2f1' in info_sel_df['Variant'].values:
-            Variant = 'r1i1p2f1'
-        elif 'r4i1p1f1' in info_sel_df['Variant'].values:
-            Variant = 'r4i1p1f1'
+            
+        if info_exp is not None:
+            info_exp_sel_df = info_exp[info_exp['Model'] == ind_mods[i]]
+            Variant = info_exp_sel_df[f'{exp}_Variant'].values[0]
         else:
-            print(set(info_sel_df['Variant']))
-            sys.exit('ERROR: Standard variant not available see list above')
+            if 'r1i1p1f1' in info_sel_df['Variant'].values:
+                Variant = 'r1i1p1f1'
+            elif 'r1i1p1f2' in info_sel_df['Variant'].values:
+                Variant = 'r1i1p1f2'
+            elif 'r1i1p1f3' in info_sel_df['Variant'].values:
+                Variant = 'r1i1p1f3'
+            elif 'r1i1p2f1' in info_sel_df['Variant'].values:
+                Variant = 'r1i1p2f1'
+            elif 'r4i1p1f1' in info_sel_df['Variant'].values:
+                Variant = 'r4i1p1f1'
+            else:
+                print(set(info_sel_df['Variant']))
+                sys.exit('ERROR: Standard variant not available see list above')
+                
         print(f'Using variant {Variant}')
         info_sel_df = info_sel_df[info_sel_df['Variant'] == Variant]
 
@@ -128,30 +136,40 @@ def make_final_info_df(info_df, ind_mods):
 
 CMIP6_path = '/nobackup_1/users/bars/synda_cmip6/CMIP6/'
 depth = depth_path(CMIP6_path)
-ens = False # True to select only r1i1p1f1, False otherwise
+# var can either the name of a specific variant like r1i1p1f1
+# or 'any' to automatically pick any variant available
+var = 'any'
 
-for var in ['zostoga', 'zos']:
-    var = [var]
+for variable in ['zostoga', 'zos']:
+    variable = [variable]
     for sce in ['historical','ssp119', 'ssp126', 'ssp245', 'ssp370', 'ssp585']:
-        print('####### Working on '+str(var)+', '+str(sce)+'#################'+
+        print('####### Working on '+str(variable)+', '+str(sce)+'#################'+
              '###############################################################')
         exp_id = [sce, 'historical', 'piControl']
-        ind_mods = select_models_intersection(CMIP6_path, exp_id, var, ens1)
+        ind_mods = select_models_intersection(CMIP6_path, exp_id, variable, var)
         print('Models available for this combination:')
         print(ind_mods)
         
         for idx, ei in enumerate(exp_id):
-            list_all_paths = select_paths(CMIP6_path, ei, var[0], ens1)
+            print(f'####### Selecting {ei} files with idx={idx}######')
+            list_all_paths = select_paths(CMIP6_path, ei, variable[0], var)
             #print('\n'.join(list_all_paths))
             info_df = make_info_df(list_all_paths, depth)
             #print('Info before final selection:')
             #print(info_df)
             if idx == 0:
-                final_info_df = make_final_info_df(info_df, ind_mods)
+                final_info_df = make_final_info_df(info_df, ind_mods, None, None)
                 final_info_df = final_info_df.rename(
                     columns={'Version':ei+'_Version', 'Variant':ei+'_Variant'})
             else:
-                v_info_df = make_final_info_df(info_df, ind_mods)
+                if var=='any' and idx==1 and sce!='historical':
+                    # Historical should use the same variant as scenario
+                    print('!!!!!! Using new case !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+                    v_info_df = make_final_info_df(info_df, ind_mods, 
+                                                   final_info_df, sce)
+                else:
+                    v_info_df = make_final_info_df(info_df, ind_mods, None, None)
+                    
                 final_info_df[ei+'_Variant'] = v_info_df.Variant
                 final_info_df[ei+'_Version'] = v_info_df.Version
         
@@ -160,14 +178,14 @@ for var in ['zostoga', 'zos']:
         print('Final info to be saved as csv file:')
         print(final_info_df)
         
-        if (len(var) == 1) & (len(exp_id) == 3):
+        if (len(variable) == 1) & (len(exp_id) == 3):
             if sce == 'historical':
-                file_name = (f'AvailableExperiments_{var[0]}_{exp_id[0]}_'+
+                file_name = (f'AvailableExperiments_{variable[0]}_{exp_id[0]}_'+
                              f'{exp_id[1]}.csv')
             else:
-                file_name = (f'AvailableExperiments_{var[0]}_{exp_id[0]}_'+
+                file_name = (f'AvailableExperiments_{variable[0]}_{exp_id[0]}_'+
                              f'{exp_id[1]}_{exp_id[2]}.csv')
             final_info_df.to_csv(file_name, index=False)
         else:
-            print('ERROR: Output file name not compatible with length of var'+
+            print('ERROR: Output file name not compatible with length of variable'+
                   'or exp_id')
